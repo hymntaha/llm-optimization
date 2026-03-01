c9d97ba105a125dfba0787c97cb8010a9ea36de8
Taha Uygun <tahauygun@gmail.com>
2026-02-28 23:34:13 -0800

    updating crew.py

--- debate/src/debate/crew.py
+++ debate/src/debate/crew.py
@@ -32,7 +32,6 @@ class Debate():
     def oppose(self) -> Task:
         return Task(
             config=self.tasks_config['oppose'], 
-            output_file='report.md'
         )
     @task
     def decide(self) -> Task:
@@ -45,9 +44,8 @@ class Debate():
         """Creates the Debate crew"""
 
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents, 
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
